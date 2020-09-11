8dad48dcbec5555fe0087a8ee101b978216e17ff
edgarjt <edgarjt97@gmail.com>
2020-09-11 02:46:26 -0500

    dashboard lte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SidebarComponent,
+    WrapperComponent,
+    ControlSidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
