ccb2689e696d2c92c1a8922db2b8e72e2d7fb113
EEFD10 <frias9701@gmail.com>
2020-10-09 00:56:28 -0500

    refactor nav

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,10 +9,10 @@ import {OfertaComponent} from './oferta/oferta.component';
 import {NotfoundComponent} from './notfound/notfound.component';
 import { ComunicacionComponent } from './comunicacion/comunicacion.component';
 import {ContabilidadComponent} from './contabilidad/contabilidad.component';
-import {DifucionComponent} from './difucion/difucion.component'
-import {ContactoComponent} from './contacto/contacto.component'
-import {IndustrialPetroleroComponent} from './industrial-petrolero/industrial-petrolero.component'
-import {PlanEstudioComponent } from  './plan-estudio/plan-estudio.component'
+import {DifucionComponent} from './difucion/difucion.component';
+import {ContactoComponent} from './contacto/contacto.component';
+import {IndustrialPetroleroComponent} from './industrial-petrolero/industrial-petrolero.component';
+import {PlanEstudioComponent } from  './plan-estudio/plan-estudio.component';
 
 /*Se crea una constante*/
 const appRoutes: Routes = [
@@ -23,9 +23,9 @@ const appRoutes: Routes = [
   {path: 'comunicacion', component: ComunicacionComponent},
   {path: 'contabilidad', component: ContabilidadComponent},
   {path: 'difucion', component: DifucionComponent},
-  {path:'contacto', component: ContactoComponent},
-  {path:'industrial_petrolero', component: IndustrialPetroleroComponent},
-  {path:'plan_estudio', component:PlanEstudioComponent },
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'industrial_petrolero', component: IndustrialPetroleroComponent},
+  {path: 'plan_estudio', component: PlanEstudioComponent },
   {path: '**', component: NotfoundComponent},
 
 
